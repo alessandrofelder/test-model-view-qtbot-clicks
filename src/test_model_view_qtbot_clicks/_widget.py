b9c9a3be50e46bddb4e1c19ad0f86dbe95596a01
alessandrofelder <alessandrofelder@users.noreply.github.com>
2023-07-19 16:05:47 +0100

    connect widget to doubleclick

--- src/test_model_view_qtbot_clicks/_widget.py
+++ src/test_model_view_qtbot_clicks/_widget.py
@@ -5,6 +5,8 @@ from qtpy.QtWidgets import QHBoxLayout, QTableView, QWidget
 if TYPE_CHECKING:
     pass
 
+from napari.utils.notifications import show_info
+
 from test_model_view_qtbot_clicks.model import RandomModel
 
 
@@ -19,3 +21,16 @@ class ExampleQWidget(QWidget):
 
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(self.view)
+
+        self.view.doubleClicked.connect(self.on_double_click)
+
+    def on_double_click(self):
+        selected_index = self.view.selectionModel().currentIndex()
+        if selected_index.isValid():
+            selected_row = selected_index.row()
+            selected_column = selected_index.column()
+            model = self.view.model()
+            selected_data = model.data(selected_index)
+            show_info(
+                f"The data at ({selected_row}, {selected_column}) is {selected_data}"
+            )
